6ba4f376ab49f005548649bda00d35f045afc1e8
Felix <felixelston07@gmail>
2024-01-13 14:58:29 -0500

    more pixy2 subsytem additions

--- src/main/java/frc/robot/subsystems/Pixy.java
+++ src/main/java/frc/robot/subsystems/Pixy.java
@@ -3,8 +3,10 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.pseudoresonance.pixy2api.Pixy2;
 
 public class Pixy extends SubsystemBase {
-    Pixy2 pixy = Pixy2.createInstance(0);
+    
+    Pixy2 pixy = new Pixy2(Pixy2.(LinkType));
     pixy.init(0);
 }
