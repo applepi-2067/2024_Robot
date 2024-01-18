071469649b00057a72e5024bc1376945e9a31aef
Felix <felixelston07@gmail>
2024-01-18 18:33:45 -0500

    Fixed SPI

--- src/main/java/frc/robot/subsystems/Pixy.java
+++ src/main/java/frc/robot/subsystems/Pixy.java
@@ -4,9 +4,15 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.links.Link;
+import io.github.pseudoresonance.pixy2api.links.SPILink;
 
 public class Pixy extends SubsystemBase {
-    
-    Pixy2 pixy = new Pixy2(Pixy2.(LinkType));
-    pixy.init(0);
+    public static void initialize() {
+        Link link = new SPILink();
+        Pixy2 pixy = Pixy2.createInstance(link);
+        pixy.init();
+        pixy.setLamp((byte) 1, (byte) 1);
+        pixy.setLED(200, 30, 255);
+    }
 }
