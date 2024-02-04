774a81225d5996a81ea3d9c1adeb27a62c1f22a2
Gordon Chen <gordonbchen@gmail.com>
2024-02-04 12:41:05 -0500

    wait until game piece isn't detected before stopping shooter

--- src/main/java/frc/robot/commands/ShootGamePiece.java
+++ src/main/java/frc/robot/commands/ShootGamePiece.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shooter;
 
@@ -12,6 +12,7 @@ public class ShootGamePiece extends SequentialCommandGroup {
       new SetShooterVelocity(Shooter.SHOOTING_SPEED_RPM, true),
       new SetFeederPercentOutput(0.3),
       
+      new WaitUntilCommand(() -> !Feeder.getInstance().gamePieceDetected()),
       new WaitCommand(0.5),  // Piece should be gone by now!
       
       new SetFeederPercentOutput(0.0),
