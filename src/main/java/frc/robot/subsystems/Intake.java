6c4ea5219378ed381bd19eb47d242324c9284fbe
Gordon Chen <gordonbchen@gmail.com>
2024-02-05 15:56:16 -0500

    use falcon500 motor for intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,22 +1,26 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.RobotMap;
 
-
 public class Intake extends SubsystemBase implements Loggable {
     private static Intake instance = null;
+    private final TalonFX m_motor;
 
-    private static final boolean INVERT_MOTOR = true;
-    private static final int CURRENT_LIMIT_AMPS = 40;
-
-    private final CANSparkMax m_motor;
+    private static final CurrentLimitsConfigs CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs()
+        .withSupplyCurrentThreshold(60)
+        .withSupplyTimeThreshold(0.5)
+        .withSupplyCurrentLimit(40);
     
     public static Intake getInstance() {
         if (instance == null) {
@@ -26,19 +30,21 @@ public class Intake extends SubsystemBase implements Loggable {
     }
 
     private Intake() {
-        m_motor = new CANSparkMax(RobotMap.canIDs.INTAKE, MotorType.kBrushless);
-        m_motor.restoreFactoryDefaults();
+        m_motor = new TalonFX(RobotMap.canIDs.INTAKE);
 
-        m_motor.setSmartCurrentLimit(CURRENT_LIMIT_AMPS);
-        m_motor.setInverted(INVERT_MOTOR);
+        m_motor.getConfigurator().apply(new TalonFXConfiguration());
+        m_motor.getConfigurator().apply(CURRENT_LIMITS_CONFIGS);
+        m_motor.getConfigurator().apply(
+            new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive)  // TODO: check inversion.
+        );
     }
 
     public void setPercentOutput(double percentOutput) {
-        m_motor.set(percentOutput);
+        m_motor.setControl(new DutyCycleOut(percentOutput));
     }
 
-    @Log (name="Output Current (A)")
+    @Log (name="Current (A)")
     public double getCurrent() {
-        return m_motor.getOutputCurrent();
+        return m_motor.getSupplyCurrent().getValueAsDouble();
     }
 }
