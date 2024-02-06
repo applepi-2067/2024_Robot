deb1e4808921f1b5861135af7ca9854257f4244f
Gordon Chen <gordonbchen@gmail.com>
2024-02-06 18:04:53 -0500

    make intake coast in neutral mode

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.RobotMap;
@@ -34,6 +35,7 @@ public class Intake extends SubsystemBase implements Loggable {
 
         m_motor.getConfigurator().apply(new TalonFXConfiguration());
         m_motor.getConfigurator().apply(CURRENT_LIMITS_CONFIGS);
+        m_motor.setNeutralMode(NeutralModeValue.Coast);
         m_motor.getConfigurator().apply(
             new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive)  // TODO: check inversion.
         );
