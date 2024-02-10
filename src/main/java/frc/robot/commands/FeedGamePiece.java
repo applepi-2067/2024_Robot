5049f1390f8256b45a564b01e5a727cb30c0d090
Gordon Chen <gordonbchen@gmail.com>
2024-02-10 16:17:27 -0500

    run intake with feeder on feeding game piece

--- src/main/java/frc/robot/commands/FeedGamePiece.java
+++ src/main/java/frc/robot/commands/FeedGamePiece.java
@@ -1,19 +1,31 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
 
 public class FeedGamePiece extends SequentialCommandGroup {
   public FeedGamePiece() {
     Feeder feeder = Feeder.getInstance();
+    Intake intake = Intake.getInstance();
 
     addCommands(
-      new SetFeederPercentOutput(0.3),
+      new ParallelCommandGroup(
+        new SetFeederPercentOutput(0.75),
+        new InstantCommand(() -> intake.setPercentOutput(0.75))
+      ),
+
       new WaitUntilCommand(feeder::gamePieceDetected),
-      new SetFeederPercentOutput(0.0)
+
+      new ParallelCommandGroup(
+        new SetFeederPercentOutput(0.0),
+        new InstantCommand(() -> intake.setPercentOutput(0.0))
+      )
     );
 
-    addRequirements(feeder);
+    addRequirements(feeder, intake);
   }
 }
