8c4776add1b4d9b78495679a1743fd432421808c
gordonbchen <gordonbchen@gmail.com>
2024-02-13 21:00:26 -0500

    remove janky reverse command in feed game piece

--- src/main/java/frc/robot/commands/FeedGamePiece.java
+++ src/main/java/frc/robot/commands/FeedGamePiece.java
@@ -1,10 +1,9 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Intake;
 
@@ -20,12 +19,10 @@ public class FeedGamePiece extends SequentialCommandGroup {
       ),
 
       new WaitUntilCommand(feeder::gamePieceDetected),
-      new InstantCommand(() -> feeder.setPercentOutput(-0.05), feeder),
-      new WaitCommand(0.05),  // Tune note position in feeder.
+      // TODO: tune note position in feeder.
 
       new ParallelCommandGroup(
-        // new SetFeederVelocity(0.0),
-        new InstantCommand(() -> feeder.setPercentOutput(0.0), feeder),
+        new SetFeederVelocity(0.0),
         new SetIntakeVelocity(0.0)
       )
     );
