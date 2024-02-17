6bfb9a9377ddf21c27e617e67cf0e0cbae2b1229
Gordon Chen <gordonbchen@gmail.com>
2024-02-17 15:10:16 -0500

    tune amp score command

--- src/main/java/frc/robot/commands/ScoreAmp.java
+++ src/main/java/frc/robot/commands/ScoreAmp.java
@@ -1,34 +1,25 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-
-import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shoulder;
 
 public class ScoreAmp extends SequentialCommandGroup {
   public ScoreAmp() {
     addCommands(
-      new ParallelCommandGroup(
-        new SetElevatorPosition(8.0, true),
-        new SetShoulderPosition(0, true)  // TODO: find shoulder angle for amp scoring.
-      ),
+      new SetShoulderPosition(78.3, true),
 
-      new SetFeederVelocity(-1_000),
+      new SetFeederVelocity(-1_000.0),
       new WaitUntilCommand(() -> !Feeder.getInstance().gamePieceDetected()),
-      new WaitUntilCommand(0.5),
+      new WaitCommand(0.5),
 
-      new ParallelCommandGroup(
-        new SetElevatorPosition(0, false),
-        new SetShoulderPosition(0, false),
-        new SetFeederVelocity(0.0)
-      )
+      new SetShoulderPosition(Shoulder.ZERO_POSITION_DEGREES, false),
+      new SetFeederVelocity(0.0)
     );
 
     addRequirements(
-      Elevator.getInstance(),
       Shoulder.getInstance(),
       Feeder.getInstance()
     );
