a1477737097686972bd34b341894de5794a15870
Gordon Chen <gordonbchen@gmail.com>
2024-02-17 13:31:22 -0500

    allow shooter to coast after shooting

--- src/main/java/frc/robot/commands/ShootGamePiece.java
+++ src/main/java/frc/robot/commands/ShootGamePiece.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
@@ -8,20 +9,20 @@ import frc.robot.subsystems.Shooter;
 
 public class ShootGamePiece extends SequentialCommandGroup {
   public ShootGamePiece() {
+    Shooter shooter = Shooter.getInstance();
+    Feeder feeder = Feeder.getInstance();
+
     addCommands(
       new SetShooterVelocity(Shooter.SHOOTING_SPEED_RPM, true),
       new SetFeederVelocity(3_000),
       
-      new WaitUntilCommand(() -> !Feeder.getInstance().gamePieceDetected()),
+      new WaitUntilCommand(() -> !feeder.gamePieceDetected()),
       new WaitCommand(0.5),  // Piece should be gone by now!
       
       new SetFeederVelocity(0.0),
-      new SetShooterVelocity(0.0, false)
+      new InstantCommand(() -> shooter.setPercentOutput(0.0), shooter)  // Coast to 0.
     );
 
-    addRequirements(
-      Feeder.getInstance(),
-      Shooter.getInstance()
-    );
+    addRequirements(feeder, shooter);
   }
 }
