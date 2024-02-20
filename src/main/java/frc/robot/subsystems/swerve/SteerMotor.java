4471ad0e823ccd5696b0c89581b0f3d9692adde4
Gordon Chen <gordonbchen@gmail.com>
2024-02-19 22:36:09 -0500

    clean up steer motor dev code

--- src/main/java/frc/robot/subsystems/swerve/SteerMotor.java
+++ src/main/java/frc/robot/subsystems/swerve/SteerMotor.java
@@ -15,15 +15,15 @@ import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class SteerMotor {
     private final TalonFX m_motor;
     private final CANcoder m_canCoder;
 
-    private final int canCoderID;
+    // private final int canCoderID;
     private final double wheelZeroOffsetDegrees;
 
     // Motor settings.
@@ -46,7 +46,7 @@ public class SteerMotor {
         .withMotionMagicAcceleration(FALCON_500_MAX_SPEED_RPS * 2.0);
 
     public SteerMotor(int canID, int canCoderID, double wheelZeroOffsetDegrees) {
-        this.canCoderID = canCoderID;
+        // this.canCoderID = canCoderID;
         this.wheelZeroOffsetDegrees = wheelZeroOffsetDegrees;
 
         // Motor.
