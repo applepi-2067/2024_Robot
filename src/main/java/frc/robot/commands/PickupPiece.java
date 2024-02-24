e75f404fc5dd599e959808ae3e9aa27677116952
Apple Pi Robotics <applepirobotics4h@gmail.com>
2024-02-24 11:31:15 -0500

    pickuppiece

---
+++ src/main/java/frc/robot/commands/PickupPiece.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shoulder;
+
+public class PickupPiece extends SequentialCommandGroup {
+  public PickupPiece() {
+    addCommands(
+      new ParallelCommandGroup(
+        new SetFeederVelocity(1_000.0),
+        new SetIntakeVelocity(3_000.0),
+        new SetShoulderPosition(Shoulder.ZERO_POSITION_DEGREES, false)
+      ),
+
+      new WaitUntilCommand(Feeder.getInstance()::gamePieceDetected),
+
+      new ParallelCommandGroup(
+        new SetFeederVelocity(0.0),
+        new SetIntakeVelocity(0.0),
+        new SetShooterVelocity(2500, false)
+      )
+    );
+
+    addRequirements(
+      Feeder.getInstance(),
+      Intake.getInstance(),
+      Shoulder.getInstance()
+    );
+  }
+}
