fc5a9bb1cbca08c52fb9f29f6e9f1694d9de2748
Gordon Chen <gordonbchen@gmail.com>
2024-03-07 16:07:28 -0500

    remove elevator todo

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -52,7 +52,7 @@ public class Elevator extends SubsystemBase implements Loggable {
   private static final double FALCON_500_MAX_SPEED_RPS = 6380.0 / 60.0;
   private static final MotionMagicConfigs MOTION_MAGIC_CONFIGS = new MotionMagicConfigs()
       .withMotionMagicCruiseVelocity(FALCON_500_MAX_SPEED_RPS)
-      .withMotionMagicAcceleration(FALCON_500_MAX_SPEED_RPS * 8.0);  // TODO: faster for amp?
+      .withMotionMagicAcceleration(FALCON_500_MAX_SPEED_RPS * 8.0);
 
   public static Elevator getInstance() {
     if (instance == null) {
