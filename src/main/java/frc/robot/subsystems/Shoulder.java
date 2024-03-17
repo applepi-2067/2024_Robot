b7f78a6c45f899b281ed508ef97b161373b1bb5a
Gordon Chen <gordonbchen@gmail.com>
2024-03-17 10:05:26 -0400

    update shoulder auto-aim calibration using only data from within shooting distance

--- src/main/java/frc/robot/subsystems/Shoulder.java
+++ src/main/java/frc/robot/subsystems/Shoulder.java
@@ -55,9 +55,9 @@ public class Shoulder extends SubsystemBase implements Loggable {
     public static final double ALLOWABLE_ERROR_DEGREES = 1.0;
 
     // Dist -> theta quadratic fit coefficients.
-    private static final double A = 0.0021422951;
-    private static final double B = -0.6794392893;
-    private static final double C = 79.24115695;  // TODO: check shoulder recalibration.
+    private static final double A = 0.0027704415;
+    private static final double B = -0.7914138823;
+    private static final double C = 83.86413741;  // TODO: check shoulder recalibration.
 
     public static Shoulder getInstance() {
         if (instance == null) {
