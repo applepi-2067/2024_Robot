05d1591f5e6b105b413d1b5b750d42b5e8d3b58c
Gordon Chen <gordonbchen@gmail.com>
2024-04-02 19:45:46 -0400

    increase feed shot speed

--- src/main/java/frc/robot/commands/FeedShot.java
+++ src/main/java/frc/robot/commands/FeedShot.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Shoulder;
 
 public class FeedShot extends SequentialCommandGroup {
@@ -13,10 +12,10 @@ public class FeedShot extends SequentialCommandGroup {
     addCommands(
         new ParallelCommandGroup(
             new SetShoulderPosition(45.0, true),
-            new SetShooterVelocity(Shooter.CLOSE_SHOOTING_SPEED_RPM, true)
+            new SetShooterVelocity(3650.0, true)
         ),
             
-        new SetFeederVelocity(3_000),
+        new SetFeederVelocity(3_000.0),
         new WaitUntilCommand(() -> !Feeder.getInstance().gamePieceDetected()),
         new WaitCommand(0.5),
 
