1ea22f5b061266e3b38307b5cb709bda47ec01ea
gordonbchen <gordonbchen@gmail.com>
2024-04-14 18:55:20 -0400

    reduce feed shot rpm

--- src/main/java/frc/robot/commands/FeedShot.java
+++ src/main/java/frc/robot/commands/FeedShot.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Shoulder;
 
 public class FeedShot extends SequentialCommandGroup {
@@ -12,7 +13,7 @@ public class FeedShot extends SequentialCommandGroup {
     addCommands(
         new ParallelCommandGroup(
             new SetShoulderPosition(45.0, true),  // TODO: check feed angle.
-            new SetShooterVelocity(3650.0, true)
+            new SetShooterVelocity(Shooter.CLOSE_SHOOTING_SPEED_RPM, true)
         ),
             
         new SetFeederVelocity(3_000.0),
