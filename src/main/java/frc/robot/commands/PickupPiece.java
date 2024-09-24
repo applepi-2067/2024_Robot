6f67bd93b4fb237c6e05f4473e8aa22fb0358c3f
Apple Pi <applePiRobot@gmail.com>
2024-09-24 17:31:08 -0400

    Fixed note not going in enough

--- src/main/java/frc/robot/commands/PickupPiece.java
+++ src/main/java/frc/robot/commands/PickupPiece.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shoulder;
@@ -12,7 +13,7 @@ public class PickupPiece extends SequentialCommandGroup {
   public PickupPiece() {
     addCommands(
       new ParallelCommandGroup(
-        new SetFeederVelocity(700.0),
+        new SetFeederVelocity(750.0),
         new SetIntakeVelocity(4_000.0),
         new SetShoulderPosition(Shoulder.MIN_ANGLE_DEGREES, true),
         new SetElevatorPosition(0.0, false, true)
@@ -20,6 +21,7 @@ public class PickupPiece extends SequentialCommandGroup {
 
       new WaitUntilCommand(Feeder.getInstance()::gamePieceDetected),
       new InstantCommand(() -> CommandScheduler.getInstance().schedule(new Rumble(0.5))),
+      new WaitCommand(0.06),
 
       new ParallelCommandGroup(
         new SetFeederVelocity(0.0),
